fa8056ce94248bf396295c0b208553f655782e91
Austin Valle <austinvalle@gmail.com>
2023-01-26 14:24:42 -0500

    ci: add `golangci-lint` + fix lints (#120)
    * ci: add `golangci-lint` + fix lints
    
    * switched order

--- internal/provider/example_resource.go
+++ internal/provider/example_resource.go
@@ -14,7 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
-// Ensure provider defined types fully satisfy framework interfaces
+// Ensure provider defined types fully satisfy framework interfaces.
 var _ resource.Resource = &ExampleResource{}
 var _ resource.ResourceWithImportState = &ExampleResource{}
 
