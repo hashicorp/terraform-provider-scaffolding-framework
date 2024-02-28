ac5d3235c506728811c960b07cbd1c916ac2102f
Brian Flad <bflad417@gmail.com>
2024-02-28 08:19:08 -0500

    Mention updating tfplugindocs command near provider address TODO

--- main.go
+++ main.go
@@ -39,6 +39,8 @@ func main() {
 
 	opts := providerserver.ServeOpts{
 		// TODO: Update this string with the published name of your provider.
+		// Also update the tfplugindocs generate command to either remove the
+		// -provider-name flag or set its value to the updated provider name.
 		Address: "registry.terraform.io/hashicorp/scaffolding",
 		Debug:   debug,
 	}
