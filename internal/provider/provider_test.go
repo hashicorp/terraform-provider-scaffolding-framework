64bb82c3b4d9bd1d9f0d464def81f97063947ae2
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-01-13 13:28:38 +0100

    provider: create a separate echo provider factory container
    Since not every acceptance test requires an ephemeral provider server for the CLI to connect to and interact with, this introduces a separate provider factory container that includes the echo provider.

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -8,14 +8,23 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
 )
 
-// testAccProtoV6ProviderFactories are used to instantiate a provider during
-// acceptance testing. The factory function will be invoked for every Terraform
-// CLI command executed to create a provider server to which the CLI can
-// reattach.
+// testAccProtoV6ProviderFactories is used to instantiate a provider during acceptance testing.
+// The factory function is called for each Terraform CLI command to create a provider
+// server that the CLI can connect to and interact with.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
+}
+
+// testAccProtoV6ProviderFactoriesWithEcho includes the echo provider alongside the scaffolding provider.
+// It allows for testing assertions on data returned by an ephemeral resource during Open.
+// The echoprovider is used to arrange tests by echoing ephemeral data into the Terraform state.
+// This lets the data be referenced in test assertions with state checks.
+var testAccProtoV6ProviderFactoriesWithEcho = map[string]func() (tfprotov6.ProviderServer, error){
+	"scaffolding": providerserver.NewProtocol6WithError(New("test")()),
+	"echo":        echoprovider.NewProviderServer(),
 }
 
 func testAccPreCheck(t *testing.T) {
