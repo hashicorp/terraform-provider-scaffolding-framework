1646a2d1daecb1a263fc267999efc45505f91a3a
tunnelWithAC <conalldalydev@gmail.com>
2025-08-09 20:41:54 +0100

    feat: add order example

---
+++ examples/order/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    hashicups = {
+      source  = "hashicorp.com/edu/hashicups"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+  host     = "http://localhost:19090"
+}
+
+resource "hashicups_order" "edu" {
+  items = [{
+    coffee = {
+      id = 3
+    }
+    quantity = 2
+    }, {
+    coffee = {
+      id = 1
+    }
+    quantity = 2
+    }
+  ]
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
+}
+
