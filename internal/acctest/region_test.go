c073bae800a798d53da78f3eeab95b26e090c6c2
Glaucio Jannotti <glaucio.jannotti@ionos.com>
2026-06-22 14:17:09 -0300

    Region datasource implemenation

---
+++ internal/acctest/region_test.go
@@ -0,0 +1,32 @@
+package acctest
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func testAccRegionDataSourceConfig() string {
+	return testAccProviderConfig() + `
+data "seca_region" "test" {
+  name = "region"
+}`
+}
+
+func TestAccRegion(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRegionDataSourceConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.seca_region.test", "name", "region"),
+					resource.TestCheckResourceAttr("data.seca_region.test", "resource_provider", "seca.region/v1"),
+					resource.TestCheckResourceAttrSet("data.seca_region.test", "available_zones.#"),
+					resource.TestCheckResourceAttrSet("data.seca_region.test", "providers.#"),
+				),
+			},
+		},
+	})
+}
---
+++ internal/provider/datasource_region.go
@@ -0,0 +1,172 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	tfschema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	sdk "github.com/eu-sovereign-cloud/go-sdk/pkg/spec/schema"
+	"github.com/eu-sovereign-cloud/go-sdk/secapi"
+)
+
+var (
+	_ datasource.DataSource              = (*RegionDataSource)(nil)
+	_ datasource.DataSourceWithConfigure = (*RegionDataSource)(nil)
+)
+
+type RegionDataSource struct {
+	client *secapi.GlobalClient
+}
+
+func newRegionDataSource() datasource.DataSource {
+	return &RegionDataSource{}
+}
+
+func (d *RegionDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_region"
+}
+
+type RegionProviderModel struct {
+	Name    types.String `tfsdk:"name"`
+	Version types.String `tfsdk:"version"`
+	Url     types.String `tfsdk:"url"`
+}
+
+var regionProviderAttrTypes = map[string]attr.Type{
+	"name":    types.StringType,
+	"version": types.StringType,
+	"url":     types.StringType,
+}
+
+type RegionDataSourceModel struct {
+	Name             types.String `tfsdk:"name"`
+	ResourceProvider types.String `tfsdk:"resource_provider"`
+	CreatedAt        types.String `tfsdk:"created_at"`
+	DeletedAt        types.String `tfsdk:"deleted_at"`
+	LastModifiedAt   types.String `tfsdk:"last_modified_at"`
+
+	AvailableZones types.List `tfsdk:"available_zones"`
+	Providers      types.List `tfsdk:"providers"`
+}
+
+func (d *RegionDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = tfschema.Schema{
+		Attributes: map[string]tfschema.Attribute{
+			"name": tfschema.StringAttribute{
+				Required: true,
+			},
+			"resource_provider": tfschema.StringAttribute{
+				Computed: true,
+			},
+			"created_at": tfschema.StringAttribute{
+				Computed: true,
+			},
+			"deleted_at": tfschema.StringAttribute{
+				Computed: true,
+			},
+			"last_modified_at": tfschema.StringAttribute{
+				Computed: true,
+			},
+			"available_zones": tfschema.ListAttribute{
+				ElementType: types.StringType,
+				Computed:    true,
+			},
+			"providers": tfschema.ListNestedAttribute{
+				Computed: true,
+				NestedObject: tfschema.NestedAttributeObject{
+					Attributes: map[string]tfschema.Attribute{
+						"name": tfschema.StringAttribute{
+							Computed: true,
+						},
+						"version": tfschema.StringAttribute{
+							Computed: true,
+						},
+						"url": tfschema.StringAttribute{
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *RegionDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	clients, ok := req.ProviderData.(clients)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected provider data type",
+			fmt.Sprintf("Expected sdk.Clients, got: %T", req.ProviderData),
+		)
+		return
+	}
+
+	d.client = clients.GlobalClient
+}
+
+func (d *RegionDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data RegionDataSourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Read the region
+
+	region, err := d.client.RegionV1.GetRegion(ctx, data.Name.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error reading region",
+			"An error was encountered when reading the region.\nError: "+err.Error(),
+		)
+		return
+	}
+
+	data, diags := regionToDataSourceModel(ctx, region)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func regionToDataSourceModel(ctx context.Context, region *sdk.Region) (RegionDataSourceModel, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	model := RegionDataSourceModel{}
+
+	model.Name = types.StringValue(region.Metadata.Name)
+	model.ResourceProvider = types.StringValue(region.Metadata.Provider)
+	model.CreatedAt = fromTime(region.Metadata.CreatedAt)
+	model.DeletedAt = fromTimePtr(region.Metadata.DeletedAt)
+	model.LastModifiedAt = fromTime(region.Metadata.LastModifiedAt)
+
+	availableZones, d := types.ListValueFrom(ctx, types.StringType, region.Spec.AvailableZones)
+	diags.Append(d...)
+	model.AvailableZones = availableZones
+
+	providers := make([]RegionProviderModel, 0, len(region.Spec.Providers))
+	for _, p := range region.Spec.Providers {
+		providers = append(providers, RegionProviderModel{
+			Name:    types.StringValue(p.Name),
+			Version: types.StringValue(p.Version),
+			Url:     types.StringValue(p.Url),
+		})
+	}
+
+	providersList, d := types.ListValueFrom(ctx, types.ObjectType{AttrTypes: regionProviderAttrTypes}, providers)
+	diags.Append(d...)
+	model.Providers = providersList
+
+	return model, diags
+}
